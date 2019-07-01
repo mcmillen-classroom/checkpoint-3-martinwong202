d130d8ca84171c2f51b5b43bd4059c04ddf5fd12
martinwong202 <martin.wong202@gmail.com>
2019-07-01 11:38:05 -0700

    overwrite

---
+++ app/src/main/java/demc/checkpoint3/MultipleChoiceQuestion.java
@@ -0,0 +1,10 @@
+package demc.checkpoint3;
+import java.util.Scanner;
+public class MultipleChoiceQuestion {
+        private String[] mOptions;
+        public MultipleChoiceQuestion (String text, String... ans){
+//            super(text);
+            mOptions = ans;
+        }
+}
+
